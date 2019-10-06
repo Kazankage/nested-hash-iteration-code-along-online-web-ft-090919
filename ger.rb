051db5904aa9b5ded74567aa3160063c29f2363a
Randel Jiao <randeljiao@gmail.com>
2019-10-06 07:25:59 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -12,9 +12,7 @@ contacts = {
 }
 
 def delete_flavor(hash, contact, flavor_to_remove)
-  hash[contact][:favorite_ice_cream_flavors].each do |flavor|
-    if flavor == flavor_to_remove
-      flavor.remove
+  hash[contact][:favorite_ice_cream_flavors].delete(flavor_to_remove)
     end
   end
   hash
